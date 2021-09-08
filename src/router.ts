800a0b493fb707527a3c4f76ebdcec917fe6fcfe
jinhuashun81 <jinhuashun81@yahoo.co.jp>
2021-09-08 22:02:24 +0900

    ページ追加

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,21 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const toppage = () => import("./pages/toppage.vue");
 const company = () => import("./pages/company.vue");
+const concept = () => import("./pages/concept.vue");
+const business = () => import("./pages/business.vue");
+const product = () => import("./pages/product.vue");
+const recruit = () => import("./pages/recruit.vue");
+const contact = () => import("./pages/contact.vue");
 const NotFound = { template: '<div>NotFound</div>' }
 
 const routes = [
   { path: "/", component: toppage },
   { path: "/company", component: company },
+  { path: "/concept", component: concept },
+  { path: "/business", component: business },
+  { path: "/product", component: product },
+  { path: "/recruit", component: recruit },
+  { path: "/contact", component: contact },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ];
 
