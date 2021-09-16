7efb6699a6bcbccdab0e501ca64519fc91d8735e
jinhuashun81 <jinhuashun81@yahoo.co.jp>
2021-09-16 16:25:15 +0900

    no message

--- src/pages/concept.vue
+++ src/pages/concept.vue
@@ -2,44 +2,39 @@
   <!-- wrapper -->
   <div id="hpb-wrapper">
     <!-- page title -->
-    <div id="hpb-title">
-      <h2><span class="ja">会社概要</span><span class="en">COMPANY</span></h2>
+    <div id="hpb-title" class="concept-image">
+      <h2><span class="ja">経営理念</span></h2>
     </div>
     <!-- page title end -->
     <!-- main -->
     <div id="hpb-main">
-      <!-- company -->
-      <div id="company">
-        <dl>
-          <dt>社名</dt>
-          <dd>株式会社同花順</dd>
-          <dt>代表取締役</dt>
-          <dd>金　花順</dd>
-          <dt>本社所在地</dt>
-          <dd>
-            〒136-0073<br />
-            東京都江東区北砂１－３－４９－４０４<br />
-            TEL. 03-6479-3143
-          </dd>
-          <dt>資本金</dt>
-          <dd>500万円</dd>
-          <dt>設立</dt>
-          <dd>2015年10月</dd>
-          <dt>取引銀行</dt>
-          <dd>みずほ銀行</dd>
-        </dl>
-        <h3>沿革</h3>
-        <dl>
-          <dt>2015年10月</dt>
-          <dd>東京都江東区北砂にて株式会社同花順を設立</dd>
-        </dl>
+      <!-- concept -->
+      <div id="concept">
+        <h4>お客様に対しては</h4>
+        <ul>
+          <li>すべてのお客様から信頼される真のパートナーシップカンパニーを目指します。</li>
+          <li>お客様のニーズに対応した最適な「人と技術と品質」のサービスを提供致します。</li>
+          <li>関係法令を遵守し、社員に対する安全及び情報保護教育を徹底します。</li>
+        </ul>
+        <h4>社員に対しては</h4>
+        <ul>
+          <li>「人がいて初めて、モノ・情報・会社を動かすことができる」との観点から自己実現の場を提供します。</li>
+          <li>安心して就業いただけるようコンプライアンスの徹底を図り、社員のステップアップをサポートします。</li>
+          <li>公平かつ適切な評価をおこない、手厚いサポートで応援します。</li>
+        </ul>
+        <h4>社員に対しては</h4>
+        <ul>
+          <li>常に公平な経営を貫き、良い企業市民を目指します。</li>
+          <li>雇用を創出し、社会に貢献致します。</li>
+          <li>グローバル社会の一員として、環境問題に積極的に取り組みます。</li>
+        </ul>
+      </div>
+      <!-- concept end -->
         <div id="pagetop">
           <a href="https://www.livetech.co.jp/company.html#hpb-container"
             >このページの先頭へ</a
           >
         </div>
-      </div>
-      <!-- company end -->
     </div>
     <!-- main end -->
   </div>
