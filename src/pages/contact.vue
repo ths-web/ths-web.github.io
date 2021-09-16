5f99dd6a747b4262ed94de42e062e0e215b8443c
jinhuashun81 <jinhuashun81@yahoo.co.jp>
2021-09-16 21:29:54 +0900

    no message

---
+++ src/pages/contact.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- wrapper -->
+  <div id="hpb-wrapper">
+    <!-- page title -->
+    <div id="hpb-title" class="contact-image">
+      <h2><span class="ja">お問い合わせ</span><span class="en">contact</span></h2>
+    </div>
+    <!-- page title end -->
+    <!-- main -->
+    <div id="hpb-main">
+      <!-- contact -->
+      <div id="contact">
+        <h3></h3>
+        <dl>
+          <dt>会社名</dt>
+          <dd>株式会社同花順</dd>
+          <dt>電話</dt>
+          <dd>03-6479-3143</dd>
+          <dt>メール</dt>
+          <dd>info@ths-web.co.jp</dd>
+          <dt>Wechat</dt>
+          <dd>shunzi55804578</dd>
+        </dl>
+        <div id="pagetop">
+          <a href="https://www.livetech.co.jp/contact.html#hpb-container"
+            >このページの先頭へ</a
+          >
+        </div>
+      </div>
+      <!-- contact end -->
+    </div>
+    <!-- main end -->
+  </div>
+  <!-- wrapper end -->
+</template>
